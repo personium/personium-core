3539b39e29f015481af0d8f1cc66755f1d380d90
fumiyasu-abe <abe.fumiyasu@coenet.co.jp>
2017-01-30 21:30:12 +0900

    #Issue-2-Cell-ACL コメント変更のみ

--- src/test/java/com/fujitsu/dc/test/jersey/cell/UpdateTest.java
+++ src/test/java/com/fujitsu/dc/test/jersey/cell/UpdateTest.java
@@ -194,7 +194,7 @@ public class UpdateTest extends AbstractCase {
         boolean existsPrivilegeAll = false;
         for (int i = 0; i < nodeListPrivilege.getLength(); i++) {
             String nodename = nodeListPrivilege.item(i).getNodeName();
-            // all -> root
+            // all -> root への対応が必要
             if ("all".equals(nodename)) {
                 existsPrivilegeAll = true;
             }
@@ -249,7 +249,7 @@ public class UpdateTest extends AbstractCase {
         existsPrivilegeAll = false;
         for (int i = 0; i < nodeListPrivilege.getLength(); i++) {
             String nodename = nodeListPrivilege.item(i).getNodeName();
-            // all -> root
+            // all -> root への対応が必要
             if ("all".equals(nodename)) {
                 existsPrivilegeAll = true;
             }
