00dff2d3a3f556579aee16802598784f2b4bf374
NISHIGUCHI Naoki <nisiguti@jp.fujitsu.com>
2017-09-21 14:14:54 +0900

    Add unit tests for issue44.

---
+++ src/test/java/io/personium/core/rs/box/ODataSvcSchemaResourceTest.java
@@ -0,0 +1,93 @@
+/**
+ * personium.io
+ * Copyright 2017 FUJITSU LIMITED
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.personium.core.rs.box;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import io.personium.core.model.DavCmp;
+import io.personium.core.model.DavRsCmp;
+import io.personium.core.odata.PersoniumODataProducer;
+import io.personium.core.rs.box.ODataSvcCollectionResource;
+import io.personium.core.rs.box.ODataSvcSchemaResource;
+import io.personium.test.categories.Unit;
+
+/**
+ * ODataSvcSchemaResource unit test classs.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ODataSvcSchemaResource.class, ODataSvcCollectionResource.class})
+@Category({ Unit.class })
+public class ODataSvcSchemaResourceTest {
+
+    /** Target class of unit test. */
+    private ODataSvcSchemaResource odataSvcSchemaResource;
+
+    /**
+     * Test constructor.
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void constructor() throws Exception {
+        DavRsCmp davRsCmp = mock(DavRsCmp.class);
+        DavCmp davCmp = mock(DavCmp.class);
+        PersoniumODataProducer producer = mock(PersoniumODataProducer.class);
+        ODataSvcCollectionResource odataSvcCollectionResource = mock(ODataSvcCollectionResource.class);
+ 
+        // --------------------
+        // Test method args
+        // --------------------
+        String url = "https://personium/cell/box/col";
+        String name = "col";
+
+        // --------------------
+        // Mock settings
+        // --------------------
+        doReturn(null).when(davRsCmp).getAccessContext();
+        doReturn(url).when(davRsCmp).getUrl();
+        doReturn(davCmp).when(davRsCmp).getDavCmp();
+        doReturn(null).when(davRsCmp).getCell();
+        doReturn(name).when(davCmp).getName();
+        doReturn(producer).when(davCmp).getSchemaODataProducer(null);
+        doReturn(null).when(producer).getMetadata();
+
+        // --------------------
+        // Expected result
+        // --------------------
+        String expected = url + "/" + name + "/";
+        
+        // --------------------
+        // Run method
+        // --------------------
+        odataSvcSchemaResource = new ODataSvcSchemaResource(davRsCmp, odataSvcCollectionResource);
+
+        // --------------------
+        // Confirm result
+        // --------------------
+        assertThat(odataSvcSchemaResource.getRootUrl(), is(expected));
+    }
+
+}
