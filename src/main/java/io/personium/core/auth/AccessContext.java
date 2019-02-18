8bc59d276e530ff3d266e37195c97637d0e85000
Wataru Sawami <fj5785cy@aa.jp.fujitsu.com>
2019-02-18 13:29:16 +0900

    #336 buf fix.

--- src/main/java/io/personium/core/auth/AccessContext.java
+++ src/main/java/io/personium/core/auth/AccessContext.java
@@ -879,16 +879,6 @@ public class AccessContext {
         }
 
         String issuer = tca.getIssuer();
-        if (!PersoniumUnitConfig.isPathBasedCellUrlEnabled()) {
-            try {
-                issuer = UriUtils.convertPathBaseToFqdnBase(issuer);
-            } catch (URISyntaxException e) {
-                // not normal issuer.
-                PersoniumCoreLog.Auth.TOKEN_PARSE_ERROR.params(e.getMessage()).writeLog();
-                return new AccessContext(TYPE_INVALID, cell, baseUri, uriInfo, InvalidReason.tokenParseError);
-            }
-        }
-
         if ((tca.getTarget().equals(baseUri) || tca.getTarget().equals(escapedBaseUri))
                 && (PersoniumUnitConfig.checkUnitUserIssuers(issuer, baseUri)
                         || PersoniumUnitConfig.checkUnitUserIssuers(issuer, escapedBaseUri))) {
