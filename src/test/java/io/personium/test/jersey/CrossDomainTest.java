10915f43f87eee0d7b725b8e87adcc4b5e2fdd05
Tochiori, Yasufumi <tochiori.y@jp.fujitsu.com>
2019-07-23 13:52:44 +0900

    Modify indents

--- src/test/java/io/personium/test/jersey/CrossDomainTest.java
+++ src/test/java/io/personium/test/jersey/CrossDomainTest.java
@@ -51,9 +51,9 @@ public class CrossDomainTest extends PersoniumTest {
     static final String ODATA_COL = "odatacol";
     static final String CROSSDOMAIN_XML = "<?xml version=\"1.0\"?>"
             + "<!DOCTYPE cross-domain-policy SYSTEM \"https://www.adobe.com/xml/dtds/cross-domain-policy.dtd\">"
-            + "  <cross-domain-policy>"
-            + "    <site-control permitted-cross-domain-policies=\"all\"/>"
-            + "    <allow-access-from domain=\"*\"/>"
+            + "<cross-domain-policy>"
+            + "  <site-control permitted-cross-domain-policies=\"all\"/>"
+            + "  <allow-access-from domain=\"*\"/>"
             + "  <allow-http-request-headers-from domain=\"*\" headers=\"*\"/>"
             + "</cross-domain-policy>";
 
