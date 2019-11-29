b1687b16f801b4a599d2ade03fd70d24a8efaeef
akioshimono <akio@shimono.net>
2019-11-29 13:22:49 +0900

    Move io related test under io.personium.test.io.* package

--- src/test/java/io/personium/test/io/model/impl/fs/DavMetadataFileTest.java
+++ src/test/java/io/personium/test/io/model/impl/fs/DavMetadataFileTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.personium.test.io.model.imple.fs;
+package io.personium.test.io.model.impl.fs;
 
 import static java.lang.ClassLoader.getSystemResourceAsStream;
 import static org.hamcrest.CoreMatchers.instanceOf;
