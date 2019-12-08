9c543890944c814a98d62d6d60a9e0ea2e6f9d81
Akio Shimono <akio@shimono.net>
2019-12-09 00:18:09 +0900

    replace the use of deprecated and broken method use with revised one.

--- src/main/java/io/personium/core/model/impl/es/CellEsImpl.java
+++ src/main/java/io/personium/core/model/impl/es/CellEsImpl.java
@@ -669,7 +669,7 @@ public class CellEsImpl extends Cell {
 
                 //Match with the rolls in the token
                 for (Role tokenRole : token.getRoleList()) {
-                    if (!tokenRole.createUrl().equals(esExtRole)) {
+                    if (!tokenRole.toRoleInstanceURL().equals(esExtRole)) {
                         continue;
                     }
                     //Look at all link information of ExtCell-Role and wash out the roll for cell which has accessed this time.
