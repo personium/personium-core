141ffe4b326eeb954b5032b77b94827feb583580
Tochiori, Yasufumi <tochiori.y@jp.fujitsu.com>
2020-03-26 10:38:16 +0900

    Fix update-version.sh

--- .github/workflows/scripts/update-version.sh
+++ .github/workflows/scripts/update-version.sh
@@ -1,27 +1,25 @@
 #!/bin/bash -xe
 
+echo 'Update version started.'
+
 MINOR_VERSION=$(
   sed -n 's|^    <version>[0-9]\+\.[0-9]\+\.\([0-9]\+\)-SNAPSHOT</version>|\1|p' pom.xml
 )
-echo ${MINOR_VERSION}
 MINOR_VERSION=$((++MINOR_VERSION))
-echo ${MINOR_VERSION}
 
 # update version in pom.xml
 sed -i \
  "s|^\(    <version>[0-9]\+\.[0-9]\+\.\)[0-9]\+-SNAPSHOT\(</version>\)|\1${MINOR_VERSION}-SNAPSHOT\2|" \
  pom.xml
 
-if [ "${COMPONENT}" = "personium-lib-common" ]; then
-  exit 0
+if [ "${COMPONENT}" = "personium-core" -o "${COMPONENT}" = "personium-engine" ]; then
+  # update version in personium-unit-config-default.properties
+  sed -i \
+    "s|^\(io\.personium\.core\.version=[0-9]\+\.[0-9]\+\.\)[0-9]\+|\1${MINOR_VERSION}|" \
+    src/main/resources/personium-unit-config-default.properties
 fi
 
-# update version in personium-unit-config-default.properties
-sed -i \
-  "s|^\(io\.personium\.core\.version=[0-9]\+\.[0-9]\+\.\)[0-9]\+|\1${MINOR_VERSION}|" \
-  src/main/resources/personium-unit-config-default.properties
-
-
+# Git commit and push
 git branch
 git --version
 git diff
