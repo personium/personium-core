dcf4a4303da18a7d356735e8e7ac7ddd28491939
Akio Shimono <akio@shimono.net>
2020-10-01 01:49:47 +0900

    resolve checkstyle warnings

--- src/test/java/io/personium/core/rs/FacadeResourceTest.java
+++ src/test/java/io/personium/core/rs/FacadeResourceTest.java
@@ -44,26 +44,39 @@ import io.personium.core.PersoniumUnitConfig;
 import io.personium.core.model.ModelFactory;
 import io.personium.core.rs.unit.UnitResource;
 
+/**
+ * Unit test class for FacadeResource.
+ *
+ */
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(ModelFactory.class)
 public class FacadeResourceTest {
     static Logger log = LoggerFactory.getLogger(FacadeResourceTest.class);
-    static volatile String shelterFqdn ;
-    
+    static volatile String shelterFqdn;
+
+    /**
+     * Class-level setup method.
+     */
     @BeforeClass
     public static void beforeClass() {
         shelterFqdn = CommonUtils.getFQDN();
         CommonUtils.setFQDN("unit.example");
         PersoniumUnitConfig.set(PersoniumUnitConfig.UNIT_PORT, "8801");
         PersoniumUnitConfig.set(PersoniumUnitConfig.UNIT_SCHEME, "https");
-
     }
+
+    /**
+     * Class-level tear-down method.
+     */
     @AfterClass
-    public static void afterClass() throws Exception {
+    public static void afterClass() {
         CommonUtils.setFQDN(shelterFqdn);
         PersoniumUnitConfig.reload();
     }
 
+    /**
+     * Test for facade() method.
+     */
     @Test
     public void facade_PathBased_ValidUrlAccess_ShouldReturn_UnitResource() {
         PersoniumUnitConfig.set(PersoniumUnitConfig.PATH_BASED_CELL_URL_ENABLED, "true");
@@ -75,6 +88,10 @@ public class FacadeResourceTest {
         };
         this.assertFacadeReturnsUnitResource(accessUrlList);
     }
+
+    /**
+     * Test for facade() method.
+     */
     @Test
     public void facade_SubdomainBased_ValidUrlAccess_ShouldReturn_UnitResource() {
         PersoniumUnitConfig.set(PersoniumUnitConfig.PATH_BASED_CELL_URL_ENABLED, "false");
@@ -89,12 +106,12 @@ public class FacadeResourceTest {
         for (URI accessUrl : accessUrlList) {
             UriInfo uriInfo = Mockito.mock(UriInfo.class);
             HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
-            
+
             Mockito.doReturn(null).when(httpServletRequest).getAttribute(anyString());
             Mockito.doReturn(accessUrl).when(uriInfo).getBaseUri();
             PowerMockito.mockStatic(ModelFactory.class);
             PowerMockito.when(ModelFactory.cellFromName(anyString())).thenReturn(null);
-            
+
             /* Target class of unit test. */
             FacadeResource facadeResource = new FacadeResource();
             Object o = facadeResource.facade(null, null, null, accessUrl.getHost(), null,
@@ -103,6 +120,9 @@ public class FacadeResourceTest {
         }
     }
 
+    /**
+     * Test for facade() method.
+     */
     @Test
     public void facade_PathBased_InvalidUrlAccess_ShouldThrow_Exception() {
         PersoniumUnitConfig.set(PersoniumUnitConfig.PATH_BASED_CELL_URL_ENABLED, "true");
@@ -114,8 +134,12 @@ public class FacadeResourceTest {
                 URI.create("https://unit.example/_ctl"),
                 URI.create("https://unit.example:8800/box/file.txt")
         };
-        this.assertFacadeThrowsPersoniumCoreException(accessUrlList, PersoniumCoreException.Common.INVALID_URL_AUTHORITY);
+        this.assertFacadeThrowsPersoniumCoreException(accessUrlList,
+                PersoniumCoreException.Common.INVALID_URL_AUTHORITY);
     }
+    /**
+     * Test for facade() method.
+     */
     @Test
     public void facade_SubdomainBased_InvalidUrlAccess_ShouldThrow_Exception() {
         PersoniumUnitConfig.set(PersoniumUnitConfig.PATH_BASED_CELL_URL_ENABLED, "false");
@@ -124,7 +148,7 @@ public class FacadeResourceTest {
                 URI.create("https://cell.unit.example:8801/_ctl")
         };
         this.assertFacadeThrowsPersoniumCoreException(accessUrlList2,  PersoniumCoreException.Dav.CELL_NOT_FOUND);
-        
+
         URI[] accessUrlList = new URI[] {
                 URI.create("https://cell.unit.example:8800/"),
                 URI.create("https://sub.cell.unit.example:8801/"),
@@ -133,19 +157,23 @@ public class FacadeResourceTest {
                 URI.create("https://unit.example/_ctl"),
                 URI.create("https://unit.example:8800/box/file.txt")
         };
-        this.assertFacadeThrowsPersoniumCoreException(accessUrlList, PersoniumCoreException.Common.INVALID_URL_AUTHORITY);
+        this.assertFacadeThrowsPersoniumCoreException(accessUrlList,
+                PersoniumCoreException.Common.INVALID_URL_AUTHORITY);
     }
-    private void assertFacadeThrowsPersoniumCoreException(URI[] accessUrlList, PersoniumCoreException expectedException) {
+
+    private void assertFacadeThrowsPersoniumCoreException(
+            URI[] accessUrlList, PersoniumCoreException expectedException) {
+
         for (URI accessUrl : accessUrlList) {
             UriInfo uriInfo = Mockito.mock(UriInfo.class);
             HttpServletRequest httpServletRequest = Mockito.mock(HttpServletRequest.class);
-            
+
             Mockito.doReturn(null).when(httpServletRequest).getAttribute(anyString());
             Mockito.doReturn(accessUrl).when(uriInfo).getBaseUri();
-            
+
             PowerMockito.mockStatic(ModelFactory.class);
             PowerMockito.when(ModelFactory.cellFromName(anyString())).thenReturn(null);
-            
+
             /* Target class of unit test. */
             FacadeResource facadeResource = new FacadeResource();
             try {
@@ -153,8 +181,9 @@ public class FacadeResourceTest {
                     null, null, null, null, uriInfo, httpServletRequest);
                 fail("Should throw Exception for URL = " + accessUrl.toString());
             } catch (PersoniumCoreException pce) {
-                assertEquals(expectedException.getCode() , pce.getCode());
+                assertEquals(expectedException.getCode(), pce.getCode());
             }
         }
     }
 }
+
