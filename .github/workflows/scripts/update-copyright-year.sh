700b3b2fc21e17dc377b3467c917ecc5a875dbdb
Tochiori Yausufmi <32325140+tochi-y@users.noreply.github.com>
2022-03-30 14:36:29 +0900

    Improve update copyright year script (#637)

--- .github/workflows/scripts/update-copyright-year.sh
+++ .github/workflows/scripts/update-copyright-year.sh
@@ -8,7 +8,12 @@ fi
 CURRENT_YEAR=$(date '+%Y')
 echo "Update Copyright year to ${CURRENT_YEAR}"
 
+# 2014 -> 2014-2022
 find "$1" -name '*' -type f \
-  | xargs sed -i -e "s|\(Copyright \)\([0-9]\{4\}-\)\?[0-9]\{4\}\( Personium\)|\1\2${CURRENT_YEAR}\3|"
+  | xargs sed -i -e "s|\(Copyright [0-9]\{4\}\)\( Personium\)|\1-${CURRENT_YEAR}\2|"
+
+# 2014-2020 -> 2014-2022
+find "$1" -name '*' -type f \
+  | xargs sed -i -e "s|\(Copyright [0-9]\{4\}-\)\?[0-9]\{4\}\( Personium\)|\1${CURRENT_YEAR}\2|"
 
 echo "Done!"
